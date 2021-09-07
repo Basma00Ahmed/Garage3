7725753bb85429f30c77e93aa03811f3ce521552
Basma Ahmed <basma00ahmed@outlook.com>
2021-09-07 15:03:55 +0200

    basma

---
+++ Garage3/Data/SeedData.cs
@@ -0,0 +1,63 @@
+﻿using Bogus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Garage3.Models.Entities;
+
+namespace Garage3.Data
+{
+    public class SeedData
+    {
+        private static Faker fake;
+
+        internal static async Task InitAsync(IServiceProvider services)
+        {
+            using(var db = services.GetRequiredService<Garage3Context>())
+            {
+                if (await db.Member.AnyAsync()) return;
+
+                fake = new Faker("sv");
+
+                var Members = GetMembers();
+                await db.AddRangeAsync(Members);
+
+                await db.SaveChangesAsync();
+            }
+        }
+
+  
+        private static List<Member> GetMembers()
+        {
+            var Members = new List<Member>();
+
+            for (int i = 0; i < 200; i++)
+            {
+                var fName = fake.Name.FirstName();
+                var lName = fake.Name.LastName();
+                var Member = new Member
+                {
+                    FirstName = fName,
+                    LastName = lName,
+                    Vehicles = new List<Vehicle>{
+                                     new Vehicle
+                                    {
+                                        RegNo = fake.Vehicle.Vin(),
+                                        Make = fake.Vehicle.Manufacturer(),
+                                        Model = fake.Vehicle.Model(),
+                                        ArrivalTime=DateTime.Now,
+                                        NoOfWheels=4,
+                                        IsCheckedOut=false,
+                                        VehicleTypeId = 1
+                                     }
+                                                }
+                };
+                Members.Add(Member);
+            }
+
+            return Members;
+        }
+    }
+}
---
+++ Garage3/Migrations/20210907103423_updatetypeid.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Garage3.Migrations
+{
+    public partial class updatetypeid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vehicle_VehicleType_VehicleTypeId",
+                table: "Vehicle");
+
+            migrationBuilder.DropColumn(
+                name: "TypeId",
+                table: "Vehicle");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VehicleTypeId",
+                table: "Vehicle",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vehicle_VehicleType_VehicleTypeId",
+                table: "Vehicle",
+                column: "VehicleTypeId",
+                principalTable: "VehicleType",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vehicle_VehicleType_VehicleTypeId",
+                table: "Vehicle");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VehicleTypeId",
+                table: "Vehicle",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TypeId",
+                table: "Vehicle",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vehicle_VehicleType_VehicleTypeId",
+                table: "Vehicle",
+                column: "VehicleTypeId",
+                principalTable: "VehicleType",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
